e76a3da41864e7eee5519f3dc89d6d0368b37a91
infoocean <infoocean8454@gmail.com>
2022-11-08 12:09:43 +0530

    update08

--- DB/db.config.connection.js
+++ DB/db.config.connection.js
@@ -1,35 +1,12 @@
-// const mongoose = require("mongoose");
-// //database info
-// const username = process.env.dbusername;
-// const password = process.env.password;
-// const dbname = process.env.dbname;
-// const db_cluster_url = `mongodb+srv://${username}:${password}@cluster0.08polbo.mongodb.net/${dbname}?retryWrites=true&w=majority`;
-// //connect database
-// mongoose
-//   .connect(db_cluster_url)
-//   .then(() => {
-//     console.log("db connection successfull");
-//   })
-//   .catch((err) => {
-//     //console.log("db connection unsuccessfull", err);
-//     console.log("db connection unsuccessfull", err.message);
-//   });
-
 const mongoose = require("mongoose");
-
 //database info
-const username = "shubhamjaiswal";
-const password = "AKoTTxQRyuU3NA1K";
-const dbname = "ecommercedatabase";
-
+const username = process.env.dbusername;
+const password = process.env.password;
+const dbname = process.env.dbname;
 const db_cluster_url = `mongodb+srv://${username}:${password}@cluster0.08polbo.mongodb.net/${dbname}?retryWrites=true&w=majority`;
-
 //connect database
 mongoose
-  .connect(db_cluster_url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(db_cluster_url)
   .then(() => {
     console.log("db connection successfull");
   })
