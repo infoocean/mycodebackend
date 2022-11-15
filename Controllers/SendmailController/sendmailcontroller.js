5283106f73ed8ac0845bfc59fb61ecf29ba6de33
infoocean <infoocean8454@gmail.com>
2022-11-15 20:05:10 +0530

    update15

--- Controllers/SendmailController/sendmailcontroller.js
+++ Controllers/SendmailController/sendmailcontroller.js
@@ -2,29 +2,29 @@ const nodemailer = require("nodemailer");
 // send mail
 const sendmailcontroller = async (req, res) => {
   console.log(req.body);
+  try {
+    let transporter = nodemailer.createTransport({
+      host: "smtp.ethereal.email",
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: "maryse.breitenberg@ethereal.email", // generated ethereal user
+        pass: "HW3N34eMMPNBEZusax", // generated ethereal password
+      },
+    });
 
-  let mailTransporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: "sj2585097@gmail.com",
-      pass: "Shubham#12",
-    },
-  });
-
-  let mailDetails = {
-    from: "sj258097@gmail.com",
-    to: "infoocean8454@gmail.com",
-    subject: "Test mail",
-    text: "Node.js testing mail for GeeksforGeeks",
-  };
-
-  mailTransporter.sendMail(mailDetails, function (err, data) {
-    if (err) {
-      console.log("Error Occurs", err);
-    } else {
-      console.log("Email sent successfully");
-    }
-  });
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: "sj2585097@gmail.com", // sender address
+      to: "infoocean8454@gmail.com", // list of receivers
+      subject: "Hello ✔", // Subject line
+      text: "Hello world?", // plain text body
+      html: "<b>Hello world?</b>", // html body
+    });
+    console.log(info);
+  } catch (error) {
+    console.log({ error: error.message });
+  }
 };
 
 module.exports = {
